3e04790b158d04dd67689e5bf3ab7528bd5e0c80
dvdmarveira <ddmdoliveira@gmail.com>
2025-04-02 01:24:50 -0300

    fix: correct sw cache behavior

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 let cacheName = "cache-v1";
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting(); // Faz o novo SW substituir o antigo imediatamente
+
   let cache = caches.open(cacheName).then((c) => {
     c.addAll([
       // nothing
@@ -10,18 +12,18 @@ self.addEventListener("install", (e) => {
   e.waitUntil(cache);
 });
 
+self.addEventListener("activate", (e) => {
+  caches.keys().then((cacheNames) => {
+    return Promise.all(
+      cacheNames.map((cache) => {
+        if (cache !== cacheName) {
+          return caches.delete(cache); // Remove caches antigos
+        }
+      })
+    );
+  });
+});
+
 self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.open(cacheName).then(function (cache) {
-      return cache.match(event.request).then(function (response) {
-        return (
-          response ||
-          fetch(event.request).then(function (response) {
-            cache.put(event.request, response.clone());
-            return response;
-          })
-        );
-      });
-    })
-  );
+  event.respondWith(fetch(event.request)); // Sempre busca arquivos novos, sem cache
 });
